4e0c04749076cf7da59b23953087319c76664d28
NatanRamosFerreira <natanprogrammer0@gmail.com>
2023-05-17 11:49:49 -0300

    aula sobre tuples

---
+++ Seção 3 = Iniciando a programação com python/aula_87.py
@@ -0,0 +1,10 @@
+'''
+
+Introdução ao desempacotamento + tuples(tuplas)
+
+'''
+
+# nomes = ['Maria', 'Bento', 'Julia']
+
+nome1, nome2, nome3 =  ['Maria', 'Bento', 'Julia']
+print(nome2)
---
+++ Seção 3 = Iniciando a programação com python/aula_88_tuples.py
@@ -0,0 +1,15 @@
+'''
+Tuples - é uma lista imutavel
+
+'''
+
+nome = ('Maria', 'Jõao', 'Pedro', 1 ) # Melhor ser especifico com os parenteses <3
+
+print(type(nome))
+print(nome)
+
+nomes = ['Chico', 'Bento', 'Junior']
+
+nomes = tuple(nomes)
+
+print(type(nomes))
