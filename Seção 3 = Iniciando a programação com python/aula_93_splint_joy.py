a4a0c3101208801f7cdad2eb80c3fab905230a5d
NatanRamosFerreira <natanprogrammer0@gmail.com>
2023-05-24 12:40:56 -0300

    testando novas funcoes

--- Seção 3 = Iniciando a programação com python/aula_93_splint_joy.py
+++ Seção 3 = Iniciando a programação com python/aula_93_splint_joy.py
@@ -3,16 +3,20 @@ split e join com list e str
 split - divide uma string (list)
 join - une uma string
 """
-palavra = 'Como Isso é curioso, e muito interessante'
+lista_palavra = '      Como Isso é curioso, e   muito interessante     '
 
-palavra_separador = palavra.split(',') # da pra separar com o ca racter que voce quer colocando ele como argumento da funcao
+palavra_separador1 = lista_palavra.split(',') # da pra separar com o ca racter que voce quer colocando ele como argumento da funcao
 
 
 # palavra_unir = palavra.join('ob')
 
 # print(palavra_unir) => realizando testes :)
+lista_frases = []
+for i, frase in enumerate(palavra_separador1):
+    lista_frases.append(palavra_separador1[i].strip()) # cortando o espaco com o strip do comeco e do fim
 
-for i, frase in enumerate(palavra_separador):
-    print(palavra_separador[1].strip()) # cortando o espaco com o strip
+# print(lista_frases)
+# print(palavra_separador1)
 
-print(palavra_separador)
+frases_unidas = '-'.join(lista_frases)
+print(frases_unidas)
