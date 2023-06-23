76259cf0cab58d0de1e59a3b57cf1db4fdc698d9
NatanRamosFerreira <natanferreira.dev@gmail.com>
2023-06-23 09:22:40 -0300

    Aula sobre Return

---
+++ Seção 4 =  Python Intermediário - Funções, Dicionários/aula_110_return.py
@@ -0,0 +1,17 @@
+'''
+Retorno de valores de funcao (return)
+'''
+
+# print('bento')
+def soma (x,y):
+    somar = x + y
+    return somar # diferente do print(que tras um none type) o return atribui um valor
+
+#var = print('bento') # print e uma funcao que so mostra um valor
+
+soma1 = soma(1,3)
+soma2 = soma(3,3)
+
+print (soma1 + soma2)
+
+# variavel = none
